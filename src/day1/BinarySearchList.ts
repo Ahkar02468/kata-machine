954da65659775464ace05ba135bc649a10c0a4eb
Ahkar02468 <arkarshwebaw@gmail.com>
2024-11-18 00:49:33 +0700

    done binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,16 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-
+     let low = 0;
+     let high = haystack.length;
+     do {
+          const m = Math.floor(low + (high - low) / 2);
+          const v = haystack[m];
+          if (v === needle) {
+               return true;
+          } else if (v > needle) {
+               high = m;
+          } else {
+               low = m + 1;
+          }
+     } while (high > low);
+     return false;
 }
